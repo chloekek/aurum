5531447b5137fb5be2166a7c876cf8aa92a54baa
Chloé Kekoa <chloekek@use.startmail.com>
2021-11-20 01:07:59 +0100

    Document conditions and features of handles

--- aurum_vm/src/heap/mod.rs
+++ aurum_vm/src/heap/mod.rs
@@ -1,4 +1,18 @@
 //! Memory allocation and garbage collection.
+//!
+//! # Handles
+//!
+//! A handle is simply a non-null pointer to an object.
+//! It also provides interior mutability, as objects are mutable.
+//! There are different types of handles.
+//! They assume different conditions and provide different sets of features.
+//! These conditions are features are summarized in the table below.
+//!
+//! | Handle type      | Conditions (cumulative)   | Provides (cumulative)                 |
+//! |------------------|---------------------------|---------------------------------------|
+//! | [`UnsafeHandle`] | None                      | Unsafe mutable access to the object   |
+//! | [`ScopedHandle`] | Object won’t be destroyed | Safe copying of parts of the object   |
+//! | [`PinnedHandle`] | Object won’t be relocated | Safe borrowing of parts of the object |
 
 pub use self::handle::*;
 pub use self::heap::*;
